a671353dd4ecb5df60a380c7c75dd02823aaf546
Istomin-1 <74534747+Istomin-1@users.noreply.github.com>
2021-04-19 09:41:10 +0500

    Refactoring code in a DetailForecastWeatherViewController.

--- Weather/ViewControllers/DetailForecastWeatherViewController.swift
+++ Weather/ViewControllers/DetailForecastWeatherViewController.swift
@@ -9,30 +9,32 @@ import UIKit
 
 class DetailForecastWeatherViewController: UIViewController {
     
+    // MARK: - Public Properties
     var forecastWeather: ForecastWeatherForCollection!
+    
+    // MARK: - Private Properties
     private let numberOfTimesOfDay = 8
     
+    // MARK: - IB Outlets
     @IBOutlet weak var currentWeatherImage: UIImageView!
     @IBOutlet weak var nameCityLabel: UILabel!
     @IBOutlet weak var currentTempLabel: UILabel!
-    
     @IBOutlet weak var feelsLikeLabel: UILabel!
     @IBOutlet weak var windLabel: UILabel!
-    
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var feelsTextLabel: UILabel!
     @IBOutlet weak var windTextLabel: UILabel!
     @IBOutlet weak var forecastTextLabel: UILabel!
     
-    
     @IBOutlet weak var forecastWeatherCollectionView: UICollectionView!
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateLayer()
         updateinterface()
         forecastWeatherCollectionView.delegate = self
         forecastWeatherCollectionView.dataSource = self
-        updateLayer()
     }
     
     func updateinterface() {
@@ -42,13 +44,14 @@ class DetailForecastWeatherViewController: UIViewController {
         feelsLikeLabel.text = forecastWeather.feelsLikeString
         windLabel.text = forecastWeather.windString
     }
+    
     func updateLayer() {
         currentWeatherImage.addShadow()
         nameCityLabel.addShadow()
         currentTempLabel.addShadow()
         feelsLikeLabel.addShadow()
         windLabel.addShadow()
-       
+        
         celsiusLabel.addShadow()
         feelsTextLabel.addShadow()
         windTextLabel.addShadow()
@@ -56,33 +59,29 @@ class DetailForecastWeatherViewController: UIViewController {
     }
 }
 
-
-
-
+// MARK: - UICollectionViewDataSource
 extension DetailForecastWeatherViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
-    func collectionView(_ collectionView: UICollectionView,
-                        viewForSupplementaryElementOfKind kind: String,
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String,
                         at indexPath: IndexPath)
     -> UICollectionReusableView {
-        
         let dateForecast = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "dateCell", for: indexPath) as! DateForecastCollectionReusableView
         
         let weatherDate = forecastWeather.dateFormater[indexPath.section * numberOfTimesOfDay]
-            
+        
         dateForecast.dateForecastLabel.text = weatherDate
-    
+        
         return dateForecast
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        1
+        
+        return 1
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return forecastWeather.dateFormater.count / numberOfTimesOfDay
     }
-    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "forecastCell", for: indexPath) as! ForecastCollectionViewCell
@@ -92,12 +91,11 @@ extension DetailForecastWeatherViewController: UICollectionViewDelegate, UIColle
         
         return cell
     }
-
 }
 
+// MARK: -  UICollectionViewDelegateFlowLayout
 extension DetailForecastWeatherViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: UIScreen.main.bounds.width, height: 105)
     }
-  
 }
--- Weather/Views/ForecastCollectionViewCell.swift
+++ Weather/Views/ForecastCollectionViewCell.swift
@@ -60,7 +60,7 @@ class ForecastCollectionViewCell: UICollectionViewCell,
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 70, height: 105)
+        return CGSize(width: 75, height: 100)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
